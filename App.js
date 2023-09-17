84e969436a7acbea05329d7350a0ee0cfb2eb063
RonaldThomasAlex <ronaldthomas094@gmail.com>
2023-09-17 22:15:52 +0530

    Adding header button

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
+import IconButton from "./UI/IconButton";
 
 const Stack = createNativeStackNavigator();
 
@@ -15,7 +16,22 @@ export default function App() {
       <StatusBar style="dark" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="AllPlaces" component={AllPlaces} />
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  size={24}
+                  color={tintColor}
+                  onPress={() => {
+                    navigation.navigate("AddPlace");
+                  }}
+                />
+              )
+            })}
+          />
 
           <Stack.Screen name="AddPlace" component={AddPlace} />
         </Stack.Navigator>
