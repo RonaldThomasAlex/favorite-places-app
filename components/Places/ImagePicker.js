c2348c8a40aaded0c19d05e90a24c7789a5a8c09
RonaldThomasAlex <ronaldthomas094@gmail.com>
2023-09-17 22:55:25 +0530

    requesting persmission for IOS

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,13 +1,45 @@
-import { Button, View } from "react-native";
-import { launchCameraAsync } from "expo-image-picker";
+import { Alert, Button, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus
+} from "expo-image-picker";
 
 const ImagePicker = () => {
+  const [cameraPermissionInfo, requestPermission] = useCameraPermissions();
+
+  async function verifyPermission() {
+    if (cameraPermissionInfo.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInfo.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions",
+        "You need to grant camera permissions to use the app"
+      );
+
+      return false;
+    }
+
+    return true;
+  }
+
   async function takeImageHandler() {
+    const hasPermission = await verifyPermission();
+
+    if (!hasPermission) {
+      return;
+    }
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5
     });
+
     console.log("image", image);
   }
 
