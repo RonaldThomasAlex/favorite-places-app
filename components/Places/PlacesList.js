de5f87c646bb00839214046db8685ff23aab3377
RonaldThomasAlex <ronaldthomas094@gmail.com>
2023-09-17 22:02:50 +0530

    Adding navigation

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -3,7 +3,7 @@ import { FlatList, View, Text, StyleSheet } from "react-native";
 import PlaceItem from "./PlaceItem";
 
 const PlacesList = ({ places }) => {
-  if (!!places || places.length === 0) {
+  if (!places || places.length === 0) {
     return (
       <View style={styles.fallbackContainer}>
         <Text style={styles.fallbackText}>
